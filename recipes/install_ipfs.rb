f26fbc9ce6331aa2f29783aadc1c0af83e4d8b6a
sponnet <stefaan@ponnet.com>
2016-12-22 21:14:05 +0100

    ipfs updqte

--- recipes/install_ipfs.rb
+++ recipes/install_ipfs.rb
@@ -3,7 +3,7 @@ script "Run a script" do
   code <<-EOH
   	cd /tmp
   	wget https://dist.ipfs.io/go-ipfs/v0.4.4/go-ipfs_v0.4.4_linux-amd64.tar.gz
-    tar xvfz go-ipfs.tar.gz
+    tar xvfz go-ipfs*.tar.gz
     mv go-ipfs/ipfs /usr/local/bin/ipfs
   EOH
 end
